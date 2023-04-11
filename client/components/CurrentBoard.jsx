fdb6458cacb99c97e75c2d57bcfe5e2912360acd
Luis Navarro <pozhiin@hotmail.com>
2023-04-10 20:47:29 -0400

    score animation

--- client/components/CurrentBoard.jsx
+++ client/components/CurrentBoard.jsx
@@ -1,7 +1,6 @@
-import React, { Component }  from "react";
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-
-
+import { motion } from 'framer-motion';
 
 const mapStateToProps = (state) => {
    return {
@@ -16,15 +15,47 @@ function mapDispatchToProps(dispatch) {
   }
 }
 
-const CurrentBoard = (props) => {
+const CurrentBoard = ({ongoingScore}) => {
+  const [particles, setParticles] = useState([]);
 
-    return (
-          <div className="scoreTank">
-            <p className="scoreName">SCORE</p>
-            <p className="ongoingScore">{props.ongoingScore}</p>
-          </div>
-    )
+  useEffect(() => {
+    setParticles([...Array(ongoingScore)].map((_, index) => index));
+  }, [ongoingScore]);
+
+  return (
+    <div className="scoreTank">
+    <p className="scoreName">SCORE</p>
+    <motion.div
+      className="explosion-container"
+      animate={{ scale: [1, 2, 3, 4, 5, 9, 20], opacity: [1, 0.3, 0] }}
+      transition={{ duration: 0.4 }}
+    >
+      <div className="explosion-particles">
+        {particles.map((particle) => (
+          <div
+            key={particle}
+            className="particle"
+            style={{
+              backgroundColor: "gold",
+              animationDelay: `${Math.random() * 0.25}s`,
+              animationDuration: `${0.5 + Math.random() * 0.5}s`,
+            }}
+          />
+        ))}
+      </div>
+      <p className="ongoingScore" style={{ position: 'absolute', top: 0, left: 0, zIndex: 2 }}>{ongoingScore}</p>           
+    </motion.div>
+    <p className="ongoingScore" style={{ position: 'absolute', top: 0, left: 0, zIndex: 1 }}>{ongoingScore}</p>
+  </div>
+  );
 }
+//     return (
+//           <div className="scoreTank">
+//             <p className="scoreName">SCORE</p>
+//             <p className="ongoingScore">{props.ongoingScore}</p>
+//           </div>
+//     )
+// }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CurrentBoard);
 
--- client/components/blocks/DynamicBlock.jsx
+++ client/components/blocks/DynamicBlock.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { TETROCOLORS } from "../../constants/tetromino";
 import { motion } from "framer-motion";
 
@@ -16,19 +16,59 @@ const DynamicBlock = ({color = '#1f2122', innerState}) => {
     };
     blockClass = `staticBlock ${tetrominoClasses[color]}`;
   }
-  // return <div className={blockClass} style={{ backgroundColor: color }} />;
 
-  return blockClass === 'hlBlock' ? (
-    <motion.div
-      className={blockClass}
-      animate={{ scale: [1, 2, 1], opacity: [1, 0, 1] }}
-      transition={{ duration: .35 }}
-      style={{ backgroundColor: color }}
-    />
+  const [particles, setParticles] = useState([]);
+  useEffect(() => {
+    const newParticles = Array.from({ length: 60 }, (_, i) => i);
+    setParticles(newParticles);
+  }, []);
+
+  useEffect(() => {
+    if (!particles.length) {
+      return;
+    }
+
+    const particleIndex = particles[0];
+    const newParticles = particles.slice(1);
+
+    const particleTimeout = setTimeout(() => {
+      setParticles(newParticles);
+    }, 750);
+
+    return () => clearTimeout(particleTimeout);
+  }, [particles]);
+
+return blockClass === "hlBlock" ? (
+<motion.div
+  className="explosion-container"
+  animate={{ 
+    scale: [1, 1.2, 1.5, 1.8, 2], 
+    opacity: [1, 0.8, 0.4, 0.2, 0] 
+  }}
+  transition={{ 
+    duration: 0.35, 
+    ease: "easeOut" 
+  }}
+>
+  <div className="explosion-color" style={{ backgroundColor: '#00e5ff'}} />
+  <div className="explosion-particles">
+    {particles.map((particle) => (
+      <div
+        key={particle}
+        className="particle"
+        style={{
+          animationDelay: `${Math.random() * 0.25}s`,
+          animationDuration: `${0.5 + Math.random() * 0.5}s`,
+        }}
+      />
+    ))}
+  </div>
+</motion.div>
+
   ) : (
     <div className={blockClass} style={{ backgroundColor: color }} />
   );
-}
+};
 
 export default DynamicBlock;
 
