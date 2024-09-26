28aa18aae0e660484dab4345a36e235a7334056f
Luis Navarro <pozhiin@hotmail.com>
2024-09-25 20:26:30 -0400

    added cors to server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require ('express');
 const app = express();
 const PORT = 3000;
 const playerController = require('./controllers/playerController.js')
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
